4173819752a76e9039f4d2daf51f4309e4cb320b
Alison Yao <yy2564@nyu.edu>
2021-12-28 16:28:12 +0400

    update keras

---
+++ Keras/temp.py
@@ -0,0 +1,5 @@
+###############################################
+# https://www.youtube.com/watch?v=qFJeN9V1ZsI&ab_channel=freeCodeCamp.org
+###############################################
+
+import tensorflow as tf
